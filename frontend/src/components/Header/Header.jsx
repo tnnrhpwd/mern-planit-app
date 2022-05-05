4934b06b04e19e1cc27bcdd6ff041aaea99b2361
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-05-05 16:17:50 -0500

    dark mode button refreshes on each click.

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,13 +5,14 @@ import HeaderLogo from './../../assets/planit192.png';
 
 function Header() {
   const [ showDropper, setShowDropper ] = useState(0);
-
+  const [ colTheme, setColTheme ] = useState(null);
 
   useEffect(() => {
 
     const body = document.body;
     const theme = localStorage.getItem('theme');
     if(theme && theme==='light-theme') {
+      setColTheme('light-theme');
       if(document.body.classList.contains('dark-theme')){
         body.classList.replace('dark-theme', 'light-theme');
       }else{
@@ -19,6 +20,7 @@ function Header() {
       }
     }
     if(theme && theme==='dark-theme') {
+      setColTheme('dark-theme');
       if(document.body.classList.contains('light-theme')){
         body.classList.replace('light-theme', 'dark-theme');
       }else{
@@ -29,6 +31,7 @@ function Header() {
   }, []);
 
   function setDarkMode(){
+    setColTheme('dark-theme');
     if(document.body.classList.contains('light-theme')){
       document.body.classList.replace('light-theme', 'dark-theme');
       if(localStorage.getItem('theme')==="light-theme") {localStorage.setItem('theme', 'dark-theme');}
@@ -38,6 +41,7 @@ function Header() {
     }
   }
   function setLightMode(){
+    setColTheme('light-theme');
     if(document.body.classList.contains('dark-theme')){
       document.body.classList.replace('dark-theme', 'light-theme');
       if(localStorage.getItem('theme')==="dark-theme") {localStorage.setItem('theme', 'light-theme'); }
@@ -65,8 +69,8 @@ function Header() {
         </div>
 
 
-        {(localStorage.getItem('theme')==="dark-theme") && <button onClick={setLightMode}>Light</button>}
-        {(localStorage.getItem('theme')==="light-theme") && <button onClick={setDarkMode}>Dark</button>}
+        {(colTheme==="dark-theme") && <button onClick={setLightMode}>Light</button>}
+        {(colTheme==="light-theme") && <button onClick={setDarkMode}>Dark</button>}
         
 
 
@@ -74,8 +78,8 @@ function Header() {
           <input id='planit-header-dropper-checkbox' type='checkbox' checked={showDropper%2===0} onChange={handleCheck}/>
           {(showDropper%2===1) &&
           <ul className='planit-header-dropper-space'>
-            {(localStorage.getItem('theme')==="dark-theme") && <button onClick={setLightMode}>Light</button>}
-            {(localStorage.getItem('theme')==="light-theme") && <button onClick={setDarkMode}>Dark</button>}
+            {(colTheme==="dark-theme") && <button onClick={setLightMode}>Light</button>}
+            {(colTheme==="light-theme") && <button onClick={setDarkMode}>Dark</button>}
             <li>Profile</li>
             <li>My Goals</li>
             <li>My Plans</li>
