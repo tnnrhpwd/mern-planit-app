85d088250fae051eb68c071d02f3375ec14678d2
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-06-01 17:22:38 -0500

    Improved the landing page for individual plans. It now has the plan data and it's comments.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- frontend/src/pages/InfoPlan/InfoPlan.jsx
+++ frontend/src/pages/InfoPlan/InfoPlan.jsx
@@ -1,56 +1,98 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'      // access state variables
 import { useParams } from "react-router-dom"
 import { toast } from 'react-toastify'                        // visible error notifications
 import { getPlans, resetPlanSlice } from './../../features/plans/planSlice'
+import { getComments, resetCommentSlice } from './../../features/comments/commentSlice'
 import Spinner from '../../components/Spinner/Spinner';
 import './InfoPlan.css';
+import CommentResult from '../../components/CommentResult/CommentResult'
 
 function InfoPlan() {
     const { id } = useParams();
     const dispatch = useDispatch();
+    const [ chosenPlan, setChosenPlan ] = useState(null);
+    const [ importedComments, setImportedComments ] = useState(null);
 
-    const { plans, planIsLoading, planIsError, planMessage } = useSelector(     // select plan values from plan state
+    const { plans, planIsLoading, planIsError, planMessage } = useSelector(     // select goal values from goal state
         (state) => state.plans
     )
-
+    const { user, authIsLoading, authIsError, authMessage } = useSelector(
+        (state) => state.auth
+    )
+    const { comments, commentIsLoading, commentIsError, commentMessage } = useSelector(     // select goal values from goal state
+    (state) => state.comments
+    )
 
 
     // called on state changes
     useEffect(() => {
-        if (planIsError) {
-        // console.log(planMessage)
-        toast.error(planMessage) // print error to toast errors
+        if ( planIsError || authIsError || commentIsError ) {
+            // console.log(planMessage)
+            toast.error(planMessage) // print error to toast errors
+            toast.error(authMessage) // print error to toast errors
+            toast.error(commentMessage) // print error to toast errors
 
         }
 
         dispatch(getPlans()) // dispatch connects to the store, then retreives the plans that match the logged in user.
+        dispatch(getComments()) // dispatch connects to the store, then retreives the plans that match the logged in user.
 
-
-        // if (!user) {            // if no user, redirect to login
-        // navigate('/login') 
-        // }
-
+  
         
         return () => {    // reset the plans when state changes
         dispatch(resetPlanSlice()) // dispatch connects to the store, then reset state values( planMessage, isloading, iserror, and issuccess )
+        dispatch(resetCommentSlice()) // dispatch connects to the store, then reset state values( planMessage, isloading, iserror, and issuccess )
         }
-    }, [planIsError, planMessage, dispatch])
+    }, [planIsError, planMessage, dispatch, authIsError, commentIsError, authMessage, commentMessage])
+
+    useEffect(() => {
+        if(!chosenPlan){
+            plans.forEach( elementPlan => {
+                if( elementPlan._id === id ){
+                    setChosenPlan( elementPlan )
+                    var outputArray = [];
+                    comments.forEach( elementComment => {
+                        if( elementComment.plan === elementPlan._id ){
+                            outputArray.push(
+                                <CommentResult key={elementComment._id} comment={elementComment}/>
+                            )
+                        }
+                    });
+                    setImportedComments(outputArray)
+                }
+            });
+        }
+    }, [chosenPlan, comments, id, plans])
 
 
-
-
-
-    if(planIsLoading){
+    if(authIsLoading){
         return(<Spinner/>)
     }
 
-    return (
-        <div className="infoplan">
-            This page contains information on plan #{id}
-        </div>
-    )
+    if(chosenPlan){
+        return (
+            <div className="infoplan">
+                <div className='infoplan-goal'>
+                    <div className='infoplan-goal-text'>
+                        { chosenPlan.goal }
+                    </div>
+                </div>                 
+                <div className='infoplan-plan'>
+                    <div className='infoplan-plan-text'>
+                        { chosenPlan.plan }
+                    </div>
+                </div> 
+                <div className='infoplan-comments'>
+                    {importedComments} 
+                </div>
+            </div>
+        )
+    }else{
+        return(<Spinner/>)
+    }
+
 }
 
 export default InfoPlan
