b1726104785a3e3da7f8177c16c05d001519eb97
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-06-01 18:22:35 -0500

    Comment results now have createdAt stamp and are visually separated.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- frontend/src/components/CommentResult/CommentResult.jsx
+++ frontend/src/components/CommentResult/CommentResult.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import CreatedAt from "../PlanResult/CreatedAt"
+import './CommentResult.css';
 
 function CommentResult(props) {
     if(props.comment.comment){
         return (
             <div className='commentresult'>
-                {props.comment.comment}
+                <div className='commentresult-time'>
+                    <CreatedAt createdAt={props.comment.createdAt}/>
+                </div>
+                <div className='commentresult-comment'>
+                    {props.comment.comment}
+                </div>
+
             </div>
         )
     }
