4cded86dbb76cb8b896fdc707ded37bbbecd2503
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-06-06 18:46:53 -0500

    Initial commit
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

---
+++ frontend/src/components/CreatePlanView/CreatePlanView.jsx
@@ -0,0 +1,11 @@
+import './CreatePlanView.css';
+
+function CreatePlanView() {
+    return (<>
+        <div className='createplanview'>
+            CreatePlanView
+        </div>
+    </>)
+}
+
+export default CreatePlanView
