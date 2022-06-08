ef379f32a6de0ff8d2c3b0594722b56f5ae29973
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-06-08 18:54:41 -0500

    Implementing similar code to the plan result.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- frontend/src/components/GoalResult/GoalResult.jsx
+++ frontend/src/components/GoalResult/GoalResult.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux'                         // useDispatch-brings in methods from state wihtout prop drilling
-import { deleteGoal } from './../../features/goals/goalSlice'
+import { updateGoal } from './../../features/goals/goalSlice'
+import ShareView from '../ShareView/ShareView.jsx'
+import { toast } from 'react-toastify'                        // visible error notifications
+import CreatedAt from '../PlanResult/CreatedAt';
+import { useNavigate } from 'react-router-dom';
+import ThumbsUp from './../../assets/thumbs-up.svg';
+import ThumbsDown from './../../assets/thumbs-down.svg';
+import ManageView from '../ManageView/ManageView';
 import './GoalResult.css';
 
-function GoalResult({ goal }) {
+function GoalResult( props ) {
   const dispatch = useDispatch()  // initialization
+  const navigate = useNavigate();
+
+  const [ shareView, setShareView ] = useState(null);
+  const [ manageView, setManageView ] = useState(null);
+
+  const goal = props.goal
+  const comments = props.comments
+
+  var user = false;
+  if( props.user ){ user = props.user }
 
   return (
     <div className='planit-goalresult'>
@@ -14,9 +32,6 @@ function GoalResult({ goal }) {
           Create Plan!
         </button>
       </a>
-      <button onClick={() => dispatch(deleteGoal(goal._id))} className='planit-goalresult-delete'>
-        Delete Goal
-      </button>
     </div>
   )
 }
