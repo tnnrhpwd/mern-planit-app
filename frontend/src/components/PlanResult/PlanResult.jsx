8d9f2e691928a16f1f5098fbdfe7be09861803bb
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-06-12 18:04:27 -0500

    The plan result component now uses plan objects from redux.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- frontend/src/components/PlanResult/PlanResult.jsx
+++ frontend/src/components/PlanResult/PlanResult.jsx
@@ -1,27 +1,83 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux'      // access state variables
-import { updatePlan } from '../../features/plans/planSlice'
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'      // access state variables
 import ShareView from '../ShareView/ShareView.jsx'
 import { toast } from 'react-toastify'                        // visible error notifications
 import CreatedAt from './CreatedAt';
 import { useNavigate } from 'react-router-dom';
+import { getPlans, resetPlanSlice, createPlan, updatePlan, deletePlan } from '../../features/plans/planSlice'
+import { getGoals, resetGoalSlice, createGoal, updateGoal, deleteGoal } from '../../features/goals/goalSlice'
+import { getComments, resetCommentSlice, createComment, updateComment } from '../../features/comments/commentSlice'
+import { getMyData, resetAuthSlice } from '../../features/auth/authSlice'
+
 import ThumbsUp from './../../assets/thumbs-up.svg';
 import ManageView from '../ManageView/ManageView';
 import ThumbsDown from './../../assets/thumbs-down.svg';
 import './PlanResult.css';
 
 function PlanResult(props) {
+  const [ plan, setPlan ] = useState(null)
+
   const dispatch = useDispatch()  // initialization
   const navigate = useNavigate();
+
+  // const { user } = useSelector((state) => state.auth)      // select user values from user state
+  const { plans, planIsLoading, planIsError, planMessage } = useSelector(     // select goal values from goal state
+    (state) => state.plans
+  )
+  const { goals, goalIsLoading, goalIsError, goalMessage } = useSelector(     // select goal values from goal state
+    (state) => state.goals
+  )
+  const { comments, commentIsLoading, commentIsError, commentMessage } = useSelector(     // select goal values from goal state
+    (state) => state.comments
+  )
+  const { user, authIsLoading, authIsError, authMessage } = useSelector(
+    (state) => state.auth
+  )
   
   const [ shareView, setShareView ] = useState(null);
   const [ manageView, setManageView ] = useState(null);
 
-  const plan = props.plan
-  const comments = props.comments
+// called on state changes
+useEffect(() => {
+    if (planIsError) {
+        toast.error(planMessage) // print error to toast errors
+    }
+    if (commentIsError) {
+        toast.error(commentMessage) // print error to toast errors
+    }
+    if (authIsError) {
+        toast.error(authMessage) // print error to toast errors
+    }
+    if (goalIsError) {
+        toast.error(goalMessage) // print error to toast errors
+    }
 
-  var user = false;
-  if( props.user ){ user = props.user }
+
+    dispatch(getPlans()) // dispatch connects to the store, then retreives the plans that match the logged in user.
+    dispatch(getGoals()) // dispatch connects to the store, then retreives the plans that match the logged in user.
+    dispatch(getComments()) // dispatch connects to the store, then retreives the plans that match the logged in user.
+    // dispatch(getMyData()) // dispatch connects to the store, then retreives the profile data that matches the logged in user.
+
+    function getPlanObjectFromObjectID(planObjectIDString){
+        return plans.find( x => x._id === planObjectIDString )
+    }
+
+    setPlan(getPlanObjectFromObjectID(props.plan))
+    console.log(getPlanObjectFromObjectID(props.plan))
+    
+    return () => {    // reset the plans when state changes
+        dispatch(resetPlanSlice()) // dispatch connects to the store, then reset state values( planMessage, planisloading, planiserror, and planissuccess )
+        dispatch(resetCommentSlice()) // dispatch connects to the store, then reset state values( commentMessage, commentisloading, commentiserror, and commentissuccess )
+        dispatch(resetGoalSlice()) // dispatch connects to the store, then reset state values( commentMessage, commentisloading, commentiserror, and commentissuccess )
+        dispatch(resetAuthSlice()) // dispatch connects to the store, then reset state values( authMessage, authisloading, authiserror, and authissuccess )
+
+    }
+}, [authIsError, authMessage, commentIsError, commentMessage, dispatch, goalIsError, goalMessage, planIsError, planMessage, plans, props.plan])
+
+
+  function getGoalObjectFromObjectID(goalObjectIDString){
+    return goals.find( x => x._id === goalObjectIDString )
+  }
 
   function handleAgree(id){
     const type = ("agree");
@@ -83,19 +139,20 @@ function PlanResult(props) {
     return ( outputArray.length )
   }
 
+  if(plan){
     return (<>
         { shareView }
         { manageView }
 
         <div key={plan._id+"0"} className='planit-planresult'>
             <div key={plan._id+"0.1"} className='planit-planresult-1'>
-                <div className='planit-planresult-date'>
-                    <CreatedAt createdAt={plan.createdAt}/>
+                <div key={plan._id+"0.11"} className='planit-planresult-date'>
+                    <CreatedAt key={plan._id+"0.12"} createdAt={plan.createdAt}/>
                 </div>
-                <div className='planit-planresult-share'>
-                    <button className='planit-planresult-share-btn' onClick={() => handleShareView("plan",plan._id)}>Share</button>
+                <div key={plan._id+"0.13"} className='planit-planresult-share'>
+                    <button key={plan._id+"0.14"} className='planit-planresult-share-btn' onClick={() => handleShareView("plan",plan._id)}>Share</button>
                 </div>
-                <div className='planit-planresult-fav'>
+                <div className='planit-planresult-fav' key={plan._id+"0.15"}>
                     { (user) ? <>{
                         <>{ (plan.followers.includes(user._id)) ?
                             <>
@@ -107,36 +164,36 @@ function PlanResult(props) {
                         }</>
                     }</>:null}
                 </div>
-                <div className='planit-planresult-manageplan'>
+                <div className='planit-planresult-manageplan' key={plan._id+"0.16"}>
                     { (user) ? <>{
-                        <button className='planit-planresult-manageplan-btn' onClick={() => handleManageView("plan",plan._id)} >☸</button>
+                        <button key={plan._id+"0.17"} className='planit-planresult-manageplan-btn' onClick={() => handleManageView("plan",plan._id)} >☸</button>
                     }</>:null}
                 </div>
             </div>
             <div key={plan._id+"0.2"} className='planit-planresult-2'>
-                <div key={plan._id+"2"} className='planit-planresult-goal'><a href={'plan/'+plan._id}><button key={plan._id+"2button"} className='planit-planresult-goalbutton'>{plan.goal}</button></a></div>
-                <div key={plan._id+"4"} className='planit-planresult-plan'><a href={'plan/'+plan._id}><button key={plan._id+"4button"} className='planit-planresult-planbutton'>{plan.plan.map(( planElement, planElementIndex ) => (<div key={plan._id+"element"+planElementIndex}>{planElement}</div>))}</button></a></div>
+                <div key={plan._id+"2"} className='planit-planresult-goal'><a href={'plan/'+plan._id}><button key={plan._id+"2button"} className='planit-planresult-goalbutton'>{getGoalObjectFromObjectID(plan.goal)}</button></a></div>
+                <div key={plan._id+"4"} className='planit-planresult-plan'><a href={'plan/'+plan._id}><button key={plan._id+"4button"} className='planit-planresult-planbutton'>{plan.plan.map(( planElement, planElementIndex ) => (<div key={plan._id+"element"+planElement}>{planElement}</div>))}</button></a></div>
             </div>
             <div key={plan._id+"0.3"} className='planit-planresult-3'>
                 <div key={plan._id+"1"} className="planit-planresult-disagree-div">
                     {(user) ?
                         <>{(plan.disusers.includes(user._id)) ?
-                            <button key={plan._id+"1button"} className='planit-planresult-disagreeACT' onClick={() => handleDisagree( plan._id )}><img className='planit-planresult-thumb' src={ThumbsDown} alt='thumbs down logo'/></button>
+                            <button key={plan._id+"1button"} className='planit-planresult-disagreeACT' onClick={() => handleDisagree( plan._id )}><img key={plan._id+"4.002"} className='planit-planresult-thumb' src={ThumbsDown} alt='thumbs down logo'/></button>
                         :
-                            <button key={plan._id+"1.5button"} className='planit-planresult-disagree' onClick={() => handleDisagree( plan._id )}><img className='planit-planresult-thumb' src={ThumbsDown} alt='thumbs down logo'/></button>
+                            <button key={plan._id+"1.5button"} className='planit-planresult-disagree' onClick={() => handleDisagree( plan._id )}><img key={plan._id+"4.001"} className='planit-planresult-thumb' src={ThumbsDown} alt='thumbs down logo'/></button>
                     }</>:null}
                 </div>
                 <div key={plan._id+"3"} className="planit-planresult-agree-div">
                     {(user) ?
                     <>{(plan.agrusers.includes(user._id)) ?
-                        <button key={plan._id+"3button"} className='planit-planresult-agreeACT' onClick={() => handleAgree( plan._id )}><img className='planit-planresult-thumb' src={ThumbsUp} alt='thumbs up logo'/></button>
+                        <button key={plan._id+"3button"} className='planit-planresult-agreeACT' onClick={() => handleAgree( plan._id )}><img key={plan._id+"4.003"} className='planit-planresult-thumb' src={ThumbsUp} alt='thumbs up logo'/></button>
                     :
-                        <button key={plan._id+"3button"} className='planit-planresult-agree' onClick={() => handleAgree( plan._id )}><img className='planit-planresult-thumb' src={ThumbsUp} alt='thumbs up logo'/></button>
+                        <button key={plan._id+"3button"} className='planit-planresult-agree' onClick={() => handleAgree( plan._id )}><img key={plan._id+"4.004"} className='planit-planresult-thumb' src={ThumbsUp} alt='thumbs up logo'/></button>
                     }</>:null}
                 </div>
-                <div className='planit-planresult-votecomment-holder' >
-                    <a href={'plan/'+plan._id} className='planit-planresult-votecomment-link'>
-                        <div className='planit-planresult-votecomment' >
+                <div className='planit-planresult-votecomment-holder' key={plan._id+"4.005"} >
+                    <a href={'plan/'+plan._id} className='planit-planresult-votecomment-link' key={plan._id+"4.006"}>
+                        <div className='planit-planresult-votecomment' key={plan._id+"4.007"} >
                             {/* Needed to add this if statement to add "+" before positive */}
                             {(plan.agrusers.length - plan.disusers.length > 0)
                                 ? "+"+(plan.agrusers.length - plan.disusers.length)+" votes "
@@ -152,6 +209,7 @@ function PlanResult(props) {
             </div>
         </div>
     </>)
+  }
 }
 
 export default PlanResult
