e1483b5330fa56878618dc870564e820468f5a46
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-06-15 08:58:43 -0500

    Comment component now works with plan array inputs.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- frontend/src/components/CommentResult/CommentResult.jsx
+++ frontend/src/components/CommentResult/CommentResult.jsx
@@ -2,14 +2,14 @@ import CreatedAt from "../PlanResult/CreatedAt"
 import './CommentResult.css';
 
 function CommentResult(props) {
-    if(props.comment.comment){
+    if(props.comment){
         return (
             <div className='commentresult'>
                 <div className='commentresult-time'>
-                    <CreatedAt createdAt={props.comment.createdAt}/>
+                    <CreatedAt createdAt={props.comment[3]}/>
                 </div>
                 <div className='commentresult-comment'>
-                    {props.comment.comment}
+                    {props.comment[1]}
                 </div>
 
             </div>
