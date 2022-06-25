dc290533286962132f32bff3cc96f4c6d50c75cb
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-06-25 18:45:42 -0500

    Action result init commit
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

---
+++ frontend/src/components/ActionResult/ActionResult.jsx
@@ -0,0 +1,11 @@
+import './ActionResult.css';
+
+function ActionResult(props) {
+  return (<>
+    <div className='actionresult'>
+      ActionResult
+    </div>
+  </>)
+}
+
+export default ActionResult
