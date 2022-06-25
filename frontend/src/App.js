12a862f2eac68fbf79ed2ac7eec87645f082cf1e
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-06-25 18:45:13 -0500

    Added InfoAction to the page list.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';    // allows frontend to send user to different pages
-import { ToastContainer } from 'react-toastify'                 // container to hold error messages        
-import 'react-toastify/dist/ReactToastify.css'    // styling
-import Header from './components/Header/Header.jsx'          // header
-import About from './pages/About/About.jsx'         // import page
-import Comments from './pages/Comments.jsx'         // import page
-import Dashboard from './pages/Dashboard/Dashboard.jsx'         // import page
-import Goals from './pages/Goals/Goals.jsx'         // import page
-import LegalTerms from './pages/LegalTerms.jsx'         // import page
-import Login from './pages/Login/Login.jsx'         // import page
-import Plans from './pages/Plans/Plans.jsx'         // import page
-import Profile from './pages/Profile/Profile.jsx'         // import page
-import Register from './pages/Register/Register.jsx'         // import page
-import Settings from './pages/Settings.jsx'         // import page
-import InfoPlan from './pages/InfoPlan/InfoPlan.jsx';
-import InfoGoal from './pages/InfoGoal/InfoGoal.jsx';
-import Agenda from './pages/Agenda/Agenda.jsx';
+import { ToastContainer } from 'react-toastify'                               // container to hold error messages        
+import 'react-toastify/dist/ReactToastify.css'                                // styling
+import Header from './components/Header/Header.jsx'                           // header
+import About from './pages/About/About.jsx'                                   // import page
+import Comments from './pages/Comments.jsx'                                   // import page
+import Dashboard from './pages/Dashboard/Dashboard.jsx'                       // import page
+import Goals from './pages/Goals/Goals.jsx'                                   // import page
+import LegalTerms from './pages/LegalTerms.jsx'                               // import page
+import Login from './pages/Login/Login.jsx'                                   // import page
+import Plans from './pages/Plans/Plans.jsx'                                   // import page
+import Profile from './pages/Profile/Profile.jsx'                             // import page
+import Register from './pages/Register/Register.jsx'                          // import page
+import Settings from './pages/Settings.jsx'                                   // import page
+import InfoGoal from './pages/InfoGoal/InfoGoal.jsx';                         // import page
+import InfoPlan from './pages/InfoPlan/InfoPlan.jsx';                         // import page
+import InfoAction from './pages/InfoAction/InfoAction.jsx';                   // import page
+import Agenda from './pages/Agenda/Agenda.jsx';                               // import page
 
 function App() {
-  return (
-  <>
+  return (<>
     <Router>
     <div className='planit-app'>
       <Header />
@@ -35,14 +35,14 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/settings' element={<Settings />} />
         <Route path='/about' element={<About />} />
-        <Route path='/plan/:id/' element={<InfoPlan />} />
         <Route path='/goal/:id/' element={<InfoGoal />} />
+        <Route path='/plan/:id/' element={<InfoPlan />} />
+        <Route path='/action/:id/' element={<InfoAction />} />
       </Routes>
     </div>
     </Router>
     <ToastContainer/>
-  </>
-  )
+  </>)
 }
 
 export default App;
