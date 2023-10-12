cdd5021fe66ffdab3324d634fdedb02be9daecad
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2023-10-11 21:28:05 -0400

    Working to implement header with working testing
    Working to implement header with working testing

--- frontend/front.test.js
+++ frontend/front.test.js
@@ -1,12 +1,18 @@
 // front.test.js
 import 'jest-localstorage-mock';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux'; // Import Provider
+import { store } from './src/app/store'; // Import your Redux store
 import App from './src/App';
 import React from 'react';
 
 describe('Frontend Testing', ()=>{
   it('has a running user-interface', () => {
-    render(<App />);
+    render(
+        <Provider store={store}>
+          <App />
+        </Provider> 
+      );
     // const linkElement = screen.getByText(/Welcome to My MERN App/i); // Adjust this to match your actual content
     // expect(linkElement).toBeInTheDocument();
   });
