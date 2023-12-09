d6e7551612ff3f79588b67011ec37bfa401e350d
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2023-12-08 19:45:32 -0500

    Delete old code

--- frontend/src/pages/Dashboard/MiddleDashboard.jsx
+++ frontend/src/pages/Dashboard/MiddleDashboard.jsx
@@ -1,36 +1,7 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { updateData, resetDataSlice } from './../../features/data/dataSlice';
-import { toast } from 'react-toastify';
 import NNetChatView from '../../components/NNetChatView/NNetChatView.jsx';
 import '../../components/NNetChatView/NNetChatView.css'
 
 function MiddleDashboard() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const [sentData, setSentData] = useState('');
-
-  async function callData() {
-    try {
-      if (sentData.trim() === '') {
-        toast.error('Please enter data to send to OpenAI.');
-        return;
-      }
-
-      dispatch(
-        updateData({
-          id: 'u',
-          data: sentData,
-        })
-      );
-    } catch (error) {
-      console.error(error);
-      toast.error('An error occurred while fetching data from OpenAI.');
-    }
-  }
-
   return (
     <div className='planit-dashboard-popular-mid'>
       <h2>ChatGPT Text Prediction</h2>
