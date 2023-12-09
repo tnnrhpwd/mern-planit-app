b1c2e846cc3e5332d8d9d281e6d743beda5b6b94
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2023-12-09 14:41:43 -0500

    Improved user interface.

--- frontend/src/components/NNetChatView/NNetChatView.jsx
+++ frontend/src/components/NNetChatView/NNetChatView.jsx
@@ -3,98 +3,121 @@ import { useDispatch, useSelector } from 'react-redux';
 import { updateData, resetDataSlice } from '../../features/data/dataSlice.js';
 import { toast } from 'react-toastify';
 import Spinner from '../Spinner/Spinner.jsx';
+import TextareaAutosize from 'react-textarea-autosize';
+
 const NNetChatView = () => {
   const dispatch = useDispatch();
   const [inputText, setInputText] = useState('');
+  const [editedText, setEditedText] = useState(''); // New state for edited content
   const [chatHistory, setChatHistory] = useState([]);
   const [editingIndex, setEditingIndex] = useState(null);
+
   // Get the relevant data from the state
   const { data, dataIsSuccess, dataIsLoading, dataIsError, dataMessage } = useSelector(
     (state) => state.data
   );
+
   // Handle data updates
   useEffect(() => {
     // If there is a new successful response, update the chat history
     if (dataIsSuccess) {
-      setChatHistory([...chatHistory, { type: 'user-message', content: inputText }, { type: 'openai-message', content: data }]);
+      if (inputText === '') {
+        setChatHistory([...chatHistory, { content: data }]);
+      } else {
+        setChatHistory([...chatHistory, { content: inputText }, { content: data }]);
+      }
       setInputText('');
     }
+
     // Handle errors
     if (dataIsError) {
       toast.error(dataMessage);
     }
+
     // Reset the data slice when unmounting or when there's an error
     return () => {
       dispatch(resetDataSlice());
     };
   }, [dispatch, dataIsSuccess, dataIsError, dataMessage, data, inputText, chatHistory]);
+
   const handleSend = async () => {
     try {
-      // Check if inputText is not empty
-      if (inputText.trim() === '') {
-        toast.error('Please enter data to send to OpenAI.');
-        return;
-      }
       // Concatenate prior messages with the current inputText
-      const combinedData = chatHistory.map(item => item.content).concat(inputText).join('\n');
-      console.log(combinedData)
+      const combinedData = chatHistory.map((item) => item.content).concat(inputText).join('\n');
+
       // Dispatch the updateData action with the inputText
-      dispatch(updateData({ id: "u", data: combinedData }));
+      dispatch(updateData({ id: 'u', data: combinedData }));
     } catch (error) {
       // Handle any errors here
       console.error(error);
       toast.error('An error occurred while fetching data from OpenAI.');
     }
   };
+
   const handleEdit = (index) => {
     setEditingIndex(index);
-    setInputText(chatHistory[index].content);
+    setEditedText(chatHistory[index].content); // Set the edited text
   };
+
   const handleSaveEdit = (index) => {
     // Update the chatHistory with the edited content
     const updatedHistory = [...chatHistory];
-    updatedHistory[index].content = inputText;
+    updatedHistory[index].content = editedText;
     setChatHistory(updatedHistory);
     setEditingIndex(null);
   };
+
   const handleCancelEdit = () => {
     setEditingIndex(null);
-    setInputText('');
+    setEditedText(''); // Reset the edited text
   };
+
+  const handleDelete = (index) => {
+    const updatedHistory = [...chatHistory];
+    updatedHistory.splice(index, 1);
+    setChatHistory(updatedHistory);
+  };
+
   const handleKeyDown = (e) => {
     if (e.key === 'Enter' && !e.shiftKey) {
       e.preventDefault(); // Prevents a new line from being added
       handleSend();
     }
   };
+
   return (
     <div className='planit-dashboard-popular-mid-chat'>
       <div className='planit-nnet-chat-history'>
         {chatHistory.map((item, index) => (
-          <div key={index} className={`planit-nnet-chat-history-${item.type}`}>
+          <div key={index} className='planit-nnet-chat-history-message'>
             {editingIndex === index ? (
               <>
-                <textarea
-                  value={inputText}
-                  onChange={(e) => setInputText(e.target.value)}
+                <TextareaAutosize
+                  value={editedText}
+                  onChange={(e) => setEditedText(e.target.value)}
                   className='planit-nnet-chat-history-edit'
                 />
-                <button onClick={() => handleSaveEdit(index)}>Save</button>
-                <button onClick={handleCancelEdit}>Cancel</button>
+                <div className='planit-nnet-chat-history-edit-buttons'>
+                  <button onClick={() => handleSaveEdit(index)}>Save</button>
+                  <button onClick={handleCancelEdit}>Cancel</button>
+                </div>
               </>
             ) : (
               <>
                 <div>{item.content}</div>
-                <button onClick={() => handleEdit(index)}>Edit</button>
+                <div className='planit-nnet-chat-history-buttons'>
+                  <button onClick={() => handleEdit(index)}>Edit</button>
+                  <button onClick={() => handleDelete(index)}>Delete</button>
+                </div>
               </>
             )}
           </div>
         ))}
       </div>
       <div className='planit-nnet-input'>
-        <textarea
+        <TextareaAutosize
           value={inputText}
-          placeholder='Please enter your message...'
+          placeholder='Input text.'
           onChange={(e) => setInputText(e.target.value)}
           onKeyDown={handleKeyDown} // Add the keydown event handler
           className='planit-dashboard-popular-mid-chat-area'
@@ -102,13 +125,14 @@ const NNetChatView = () => {
         <button
           onClick={handleSend}
           disabled={dataIsLoading}
-          id='planit-dashboard-popular-mid-chat-button'
+          id='planit-dashboard-popular-mid-chat-gobutton'
         >
-          Send ⚡
+          Go ⚡
         </button>
       </div>
       {dataIsLoading && <Spinner />}
     </div>
   );
 };
+
 export default NNetChatView;
--- frontend/src/pages/Dashboard/MiddleDashboard.jsx
+++ frontend/src/pages/Dashboard/MiddleDashboard.jsx
@@ -4,10 +4,8 @@ import '../../components/NNetChatView/NNetChatView.css'
 function MiddleDashboard() {
   return (
     <div className='planit-dashboard-popular-mid'>
-      <h2>ChatGPT Text Prediction</h2>
-      <div >
+      <h2>Text Prediction</h2>
         <NNetChatView />
-      </div>
     </div>
   );
 }
