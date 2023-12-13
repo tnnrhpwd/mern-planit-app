9978ffc1710cc8089eb62dcae3e9f1c3f6c5a4df
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2023-12-13 18:36:47 -0500

    Implementing NNet chat history to be able to remember chats from prior sessions.
    It will utilize the backend to save the chat to a database entry. it would be most efficient to only communicate the newest string from the database, so both user and backend may have to  keep histories parallel to each other as to never send the whole chathistory at once.

--- frontend/src/pages/Net/Net.jsx
+++ frontend/src/pages/Net/Net.jsx
@@ -1,9 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateData, resetDataSlice } from '../../features/data/dataSlice.js';
+import { toast } from 'react-toastify';
 import NNetChatView from '../../components/NNetChatView/NNetChatView.jsx';
 import '../../components/NNetChatView/NNetChatView.css'
 
 function Net() {
+  const [bookIsOpen, setBookIsOpen] = useState(false);
+
+    // Get the relevant data from the state
+    const { data, dataIsSuccess, dataIsLoading, dataIsError, dataMessage } = useSelector(
+      (state) => state.data
+    );
+  const handleBook = async () => {
+    try {
+      if(bookIsOpen){setBookIsOpen(false)}else{setBookIsOpen(true)}
+    } catch (error) {
+      // Handle any errors here
+
+    }
+  };
   return (
     <div className='planit-dashboard-popular-mid'>
+            <button          
+          onClick={handleBook}
+          disabled={dataIsLoading}
+          id='planit-dashboard-popular-mid-chat-bookbutton'
+        >
+          📕{bookIsOpen}
+        </button>
         <NNetChatView />
     </div>
   );
